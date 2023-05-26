4b1dea52e3ac7f5317af0b942c234a19a75fee7c
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2023-05-26 12:35:36 +0200

    modal closing now works

--- scripts.js
+++ scripts.js
@@ -25,15 +25,11 @@ function close(obj) {
   console.log(modal);
   modal.style.display = "none";
 }
-button.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == button) {
+  if (event.target == modal) {
     modal.style.display = "none";
-  }
+   }
 }
