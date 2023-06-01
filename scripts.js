9dc0412949b923bf8b37a78793d12abbf342f6d1
terris03 <111565076+terris03@users.noreply.github.com>
2023-06-01 13:05:49 +0200

    hamburger menu now works
    Co-Authored-By: Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -30,4 +30,6 @@ window.onclick = function (event) {
 
 function LarsNilsFunction(x) {
   x.classList.toggle("change");
+  let dropdown = document.getElementById('hamburger-items-container');
+  dropdown.classList.toggle("show");
 }
