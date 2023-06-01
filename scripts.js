ef9722e61083a2b1abde274c3467e41c2250dd61
terris03 <111565076+terris03@users.noreply.github.com>
2023-06-01 13:38:09 +0200

    now animates dropdown
    Co-Authored-By: Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -32,4 +32,6 @@ function LarsNilsFunction(x) {
   x.classList.toggle("change");
   let dropdown = document.getElementById('hamburger-items-container');
   dropdown.classList.toggle("show");
+  dropdown.classList.toggle("dropped");
+  
 }
